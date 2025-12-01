697cdefb58c93cb16ba120c313ba04aeb1fe8370
Stafford Williams <stafford.williams@gmail.com>
2025-12-01 22:13:07 +1100

    empty refactor

--- src/commands/refactor/refactor.ts
+++ src/commands/refactor/refactor.ts
@@ -1,11 +1,4 @@
-import { runFileGrowthCheck } from "./fileGrowthCheck";
-import { runFileNameCheck } from "./fileNameCheck";
 
 export function refactor(): void {
-	const fileGrowthPassed = runFileGrowthCheck();
-	const fileNamePassed = runFileNameCheck();
 
-	if (!fileGrowthPassed || !fileNamePassed) {
-		process.exit(1);
-	}
 }
