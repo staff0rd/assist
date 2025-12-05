c17ac7a138beeeb5d36231cda26c63e6639a2950
Stafford Williams <stafford.williams@gmail.com>
2025-12-05 22:22:39 +1100

    remove refactor check next command

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@
 import { Command } from "commander";
 import { commit } from "./commands/commit";
 import { lint } from "./commands/lint/lint";
-import { next as refactorCheckNext } from "./commands/refactor/next";
 import {
 	check as refactorCheck,
 	ignore as refactorIgnore,
@@ -38,15 +37,10 @@ const refactorCommand = program
 	.command("refactor")
 	.description("Run refactoring checks for code quality");
 
-const refactorCheckCommand = refactorCommand
+refactorCommand
 	.command("check [pattern]")
 	.description("Check for files that exceed 100 lines")
 	.action(refactorCheck);
-
-refactorCheckCommand
-	.command("next")
-	.description("Show the next file to refactor (largest violation)")
-	.action(refactorCheckNext);
 
 refactorCommand
 	.command("ignore <file>")
