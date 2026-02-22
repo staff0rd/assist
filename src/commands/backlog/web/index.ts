f33782ef318aef3b0c1bdfa66b81e8e25c87f806
Stafford Williams <stafford.williams@gmail.com>
2026-02-22 14:33:02 +1100

    feat: open browser on backlog web

--- src/commands/backlog/web/index.ts
+++ src/commands/backlog/web/index.ts
@@ -1,16 +1,19 @@
+import { exec } from "node:child_process";
 import { createServer } from "node:http";
 import chalk from "chalk";
 import { handleRequest } from "./handleRequest";
 
 export async function web(options: { port: string }): Promise<void> {
 	const port = Number.parseInt(options.port, 10);
+	const url = `http://localhost:${port}`;
 
 	const server = createServer((req, res) => {
 		handleRequest(req, res, port);
 	});
 
 	server.listen(port, () => {
-		console.log(chalk.green(`Backlog web view: http://localhost:${port}`));
+		console.log(chalk.green(`Backlog web view: ${url}`));
 		console.log(chalk.dim("Press Ctrl+C to stop"));
+		exec(`open ${url}`);
 	});
 }
