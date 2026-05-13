e853e0552fbf8aec66e5e0a53067702678e82037
Stafford Williams <stafford.williams@gmail.com>
2026-05-13 23:01:11 +1000

    feat: add backlog stop command

---
+++ src/commands/backlog/stop/index.ts
@@ -0,0 +1,23 @@
+import chalk from "chalk";
+import { loadBacklog, saveBacklog } from "../shared";
+
+export async function stop(): Promise<void> {
+	const items = loadBacklog();
+	const inProgress = items.filter((item) => item.status === "in-progress");
+
+	if (inProgress.length === 0) {
+		console.log(chalk.yellow("No in-progress items to stop."));
+		return;
+	}
+
+	for (const item of inProgress) {
+		item.status = "todo";
+		item.currentPhase = 1;
+	}
+
+	saveBacklog(items);
+
+	for (const item of inProgress) {
+		console.log(chalk.yellow(`Stopped item #${item.id}: ${item.name}`));
+	}
+}
